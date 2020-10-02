18c6dec1671d462e8318f0b487914de86f13681d
genkina <cezarcqc@gmail.com>
2020-10-02 19:10:37 -0300

    Add files via upload

---
+++ Models/Interface/Interface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jogoEmprestado.Models.Interface
+{
+    interface Interface<T>
+    {
+        public T GetT();
+
+
+    }
+}
---
+++ Models/jogo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jogoEmprestado.Models
+{
+    public class Jogo
+    {
+        public string nome { get; set; }
+        public string consolePc { get; set; }
+        //data prometida da devolução ao amigo
+        public DateTime entrega { get; set; }
+    }
+}
